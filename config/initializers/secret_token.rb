bd87fea22a7500cab5b98baa564109f700b56ee5
Mitchel Kuijpers <mitchelkuijpers@gmail.com>
2011-10-14 13:55:57 +0200

    this really fixes #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buddylist::Application.config.secret_token = '439d5fad6269556b0f48beb675b68796043ad9fb2f531774b23494a50dceb39a5bed5c61966b0be11e629b4502903a8c2ee2e50f74e7d616c418dd60e3fa485a'
