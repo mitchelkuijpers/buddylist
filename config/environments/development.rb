e6a0ee72aed6e1d684c72b23905896c8177eabe3
Teun van Vegchel <teun@veggle.nl>
2011-10-27 15:25:37 +0200

    Initial configuration required for Devise
    Some basic application configuration required for Devise to operate
    properly.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ Buddylist::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Default action mailer url option
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Buddylist::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  root :to => "people#index"
 
   get 'person/:person_id(.:format)' => 'people#view', :as => :person
   post 'status/create' => 'statusSharables#create', as: :create_status_sharable
