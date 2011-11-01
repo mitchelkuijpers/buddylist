ad8eedbb78b4e4607e3aaafa7a69c2ae058306c5
Teun van Vegchel <teun@veggle.nl>
2011-11-01 21:54:38 +0100

    Fixes #9; Added 'Document not found' errors for pages that throw Mongoids record not found exception.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
+  rescue_from Mongoid::Errors::DocumentNotFound, with: :document_not_found
+
   protect_from_forgery
   before_filter :console_line
 
-  private
+
+  protected
+
 
   def console_line
-    puts "\n\n\n\n!!! ======================================================== !!!\n\n\n\n" # TODO Remove - Used for visibility in console due to asset errors
+    # TODO Remove - Used for visibility in console due to asset errors
+    puts "\n\n\n\n!!! ======================================================== !!!\n\n\n\n"
+  end
+
+
+  def document_not_found
+    render "common/document_not_found"
   end
 
 end
